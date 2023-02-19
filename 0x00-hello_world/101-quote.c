440b5bc8487c21dd5d6e15da48808b1be39a58ea
adigun lukman <adigun055@gmail.com>
2023-02-19 01:08:58 -0800

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description: Standard error
  *
- * Retur: Always return 0 (Success)
+ * Return: Always return 0 (Success)
  */
 
-int main (void)
-{	
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+int main(void)
+{
+	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
 
