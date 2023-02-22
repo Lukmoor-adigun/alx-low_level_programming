57becf9cf2c7247bd55a9ef9201254b739845bf0
adigun lukman <adigun055@gmail.com>
2023-02-21 18:12:48 -0800

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description: program that prints _putchar
+ *
+ * Return: ALways 0 on success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
