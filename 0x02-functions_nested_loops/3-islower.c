f59cf7c22340adeaaa59aa2e58494025e7008b8c
adigun lukman <adigun055@gmail.com>
2023-02-22 19:00:51 -0800

    did betty a service

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,18 +4,18 @@
 /**
  * _islower - checks if a character is low or upp letters
  *
- * Result: returns 1 on success 0 otherwise
+ * Return: returns 1 on success 0 otherwise
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
 
