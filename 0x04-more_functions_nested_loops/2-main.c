6c1058ddfe00490d5899a2e3d26a0fbe483dba2f
adigun lukman <adigun055@gmail.com>
2023-02-23 15:25:11 -0800

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * return: ALways 0
+ */
+
+int main(void)
+{
+	printf("%d\n", mul(10, 20));
+	return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ *
+ * @a: character to multiply
+ * @b: character to multiply
+ *
+ * Return: a x b
+ */
+
+int mul(int a, int b)
+{
+	int multiply;
+
+	multiply = a * b;
+	return (multiply);
+}
+
