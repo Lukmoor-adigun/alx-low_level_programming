61deeaf9b944dbd497ecec63e065afced4d957e5
adigun lukman <adigun055@gmail.com>
2023-02-27 12:25:47 -0800

    Learning c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Implementation of puchar
+ *
+ * Retuen: always return putchar
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Learning C
+learning c
