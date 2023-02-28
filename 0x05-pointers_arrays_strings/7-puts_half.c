6b0f10d7c6fe4bc12bd094a7aadc643da11b996d
adigun lukman <adigun055@gmail.com>
2023-02-27 18:31:07 -0800

    prints the second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes a character to stdout.
+ *
+ * @c: the character to write
+ */
+
+void _putchar(char c);
+/**
+ * puts-half - prints the second half of a string followed by new line
+ *
+ * @str: The string to print
+ */
+
+void puts_half(char *str)
+{
+	int length - strlen(str);
+	int start = length / 2;
+
+	if (length % 2 == 1)
+	{
+		start = (length - 1) / 2;
+	}
+
+	int i;
+
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
