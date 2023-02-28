dbe092f4a5c72ef5c416ffa2330e00656bf7d8c4
adigun lukman <adigun055@gmail.com>
2023-02-27 18:37:22 -0800

    second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * _putchar - writes a character to stdout.
@@ -18,13 +19,12 @@ void puts_half(char *str)
 {
 	int length = strlen(str);
 	int start = length / 2;
+	int i;
 
 	if (length % 2 == 1)
 	{
 		start = (length - 1) / 2;
 	}
-
-	int i;
 
 	for (i = start; i < length; i++)
 	{
