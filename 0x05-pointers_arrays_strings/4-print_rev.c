ee2e363c3b9bfc18867eba2a2e8b1308a314f650
adigun lukman <adigun055@gmail.com>
2023-02-27 17:44:33 -0800

    prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 
 /**
+ * _putchar - writes a character to stdout.
+ *
+ * @c: the character to write
+ */
+
+void _putchar(char c);
+/**
  * print_rev - Prints string in reverse
  *
  * @s: The string to print
@@ -11,8 +18,9 @@
 void print_rev(char *s)
 {
 	int length = strlen(s);
+	int i;
 
-	for (int i = length - 1; i >= 0; i++)
+	for (i = length - 1; i >= 0; i++)
 	{
 		_putchar(s[i]);
 	}
