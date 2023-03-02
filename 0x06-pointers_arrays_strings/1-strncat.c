735aeca5287d799b51e1fe3a8d92e3b4c00ff1c7
adigun lukman <adigun055@gmail.com>
2023-03-02 12:53:44 -0800

    concatenate string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,9 @@
 #include <string.h>
 
 /**
- * _strncat - function that concatenates two strings
+ * _strncat - The _strncat function is similar to the _strcat function
+ *  except that it will use at most n bytes from src; and src does not
+ *  need to be null-terminated if it contains n or more bytes
  * @src: where to check for n bytes
  * @n: bytes
  * @dest: resulting string
