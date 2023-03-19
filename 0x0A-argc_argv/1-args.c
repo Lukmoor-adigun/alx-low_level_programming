c9f6dcdf8ef4d90261c6177aee49fd1590bdbbe5
adigun lukman <adigun055@gmail.com>
2023-03-18 23:01:00 -0700

    count number of arguments poassed to argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints number of arguments passed int it
+ * @argc: command line argument
+ * @argv: pointer to array to store the strings in argc
+ * result: 0 on succes
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
