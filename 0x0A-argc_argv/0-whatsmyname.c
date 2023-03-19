66a2af0804e6f4246a531b03aa4a67d5e746a224
adigun lukman <adigun055@gmail.com>
2023-03-18 21:52:41 -0700

    program that prints name of exe

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * main - program that prints its name followed by a new line
+ * @argc: command line arg
+ * @argv: pointer to the arg of argc
  *
  * Return: 0 on success.
  */
-int main(void)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("The value of argc is %d", 5);
+	printf("%s\n", argv[0]);
 	return (0);
 }
