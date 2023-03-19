18da2077575295ddfa6506615d5c517e07f72bee
adigun lukman <adigun055@gmail.com>
2023-03-18 21:34:21 -0700

    testing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name followed by a new line
+ *
+ * Return: 0 on success.
+ */
+int main()
+{
+	printf("The value of argc is %d", 5);
+	return (0);
+}
