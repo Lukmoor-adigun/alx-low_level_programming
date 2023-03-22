1171428d0730f6c921e0d4d3f67ca29cd07e867b
adigun lukman <adigun055@gmail.com>
2023-03-22 16:42:06 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog details
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: name of the owner of dog
+ *
+ * Description: store details of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* end DOG_H */
