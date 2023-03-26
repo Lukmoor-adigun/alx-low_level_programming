f6eee78b82d89ab13b85a637a8196c24cb4831e9
adigun lukman <adigun055@gmail.com>
2023-03-26 03:43:49 -0700

    Write a function that concatenates all the arguments of your program.
    Prototype: char *argstostr(int ac, char **av);
    Returns NULL if ac == 0 or av == NULL
    Returns a pointer to a new string, or NULL if it fails
    Each argument should be followed by a \n in the new string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * argstostr - concatenates all the arguments of the program.
+ *
+ * @ac: the number of arguments.
+ * @av: the array of arguments.
+ *
+ * Return: a pointer to a new string that contains the concatenated arguments,
+ *         or NULL if it fails.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	/* Calculate the length of the concatenated string */
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+	/* Allocate memory for the concatenated string */
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+	/* Concatenate the arguments into the string */
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
